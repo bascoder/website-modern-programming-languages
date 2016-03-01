a22eac3e31562af4e9dde0605d5210938c8d0d2a
Bas van Marwijk <basvmar@hotmail.com>
2016-03-01 10:13:55 +0100

    HTML5 history api instead of duplicated html files

--- js/main.js
+++ js/main.js
@@ -13,16 +13,25 @@
 
         var currentPage;
 
-        function initNav() {
-            function setActiveLi() {
-                $('#general-nav').find('a').each(function () {
-                    if ($(this).attr('href') == currentPage) {
-                        $(this).parent().addClass('active');
-                    }
-                });
-            }
+        function setActiveLi() {
+            $('#general-nav').find('a').each(function () {
+                if ($(this).attr('href') == currentPage) {
+                    $(this).parent().addClass('active');
+                } else {
+                    $(this).parent().removeClass('active');
+                }
+            });
+        }
 
-            appendTemplate('_nav.html', setActiveLi);
+        function initNav() {
+
+            appendTemplate('_nav.html', function () {
+                $('#general-nav').find('a').each(function () {
+                    addClicker(this);
+                });
+
+                setActiveLi();
+            });
         }
 
         function initMainContent() {
@@ -41,6 +50,9 @@
         function setCurrentPage() {
             var pathParts = window.location.pathname.split('/');
             currentPage = pathParts[pathParts.length - 1];
+            if (!currentPage || currentPage.length == 0 || currentPage == '#') {
+                currentPage = 'index.html';
+            }
         }
 
         this.init = function () {
@@ -48,8 +60,46 @@
 
             initNav();
             initMainContent();
+        };
+
+        this.swapPage = function () {
+            setCurrentPage();
+
+            console.log("New page: ", currentPage);
+            setActiveLi();
+        };
+    };
+
+    var Navigation = function () {
+
+        this.navigate = function (link) {
+            console.log("Pushing state: ", link.href);
+            history.pushState(null, null, link.href);
+            page.swapPage();
+        };
+
+        this.init = function () {
+            window.onpopstate = page.swapPage;
         }
     };
 
-    new Page().init();
+    function addClicker(link) {
+        link.addEventListener("click", function (e) {
+            try {
+                navigation.navigate(link);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                e.preventDefault();
+                e.stopPropagation();
+            }
+
+        }, false);
+    }
+
+    var page = new Page();
+    var navigation = new Navigation();
+
+    page.init();
+    navigation.init();
 })();
